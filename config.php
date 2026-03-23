87b305492fce53595d619403b80fc4d14e195adf
Jahnavi Srivastava <jahnavisrivastava10@gmail.com>
2026-03-23 20:01:30 +0530

    config.php

--- config.php
+++ config.php
@@ -1,15 +1,18 @@
 <?php
-   $servername="localhost";
-   $username="root";
-   $password="";
-   $dbname="fitness_app";
+$servername = "sql100.infinityfree.com";
+$username   = "if0_41380838";
+$password   = "AV3Wnkl97y9";
+$dbname     = "if0_41380838_fitness_app";
 
-   $conn = mysqli_connect($servername,$username,$password,$dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-   
-   
-   if(!$conn){
-      die("Connection failed".mysqli_connect_error());
-   }
-
+if (!$conn) {
+    header('Content-Type: application/json');
+    http_response_code(500);
+    echo json_encode([
+        "status" => 0,
+        "message" => "Database connection failed: " . mysqli_connect_error()
+    ]);
+    exit;
+}
 ?>
